d41c8365c1d97b3d1321cfca61d84594a0fa190c
name <email>
2014-04-30 02:18:29 -0400

    Added line to production to see if heroku worked

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,7 @@ SampleApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  ENV["REDISTOGO_URL"] = 'redis://redistogo:5968e16f1b51903c16e949f7a9dd1863@grideye.redistogo.com:10683'
+  
 end
