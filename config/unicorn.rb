902ff0648e3651972bc2c240dcd79e365c7ea964
name <email>
2014-05-01 15:39:50 -0400

    cahgned unicorn file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,12 @@
+require 'sidekiq'
 before_fork do |server, worker|
    @sidekiq_pid ||= spawn("bundle exec sidekiq -c 3")
 end
 
-worker_processes 3
+worker_processes 2
+
 after_fork do |server, worker|
   Sidekiq.configure_client do |config|
     config.redis = { :size => 1 }
-  end
-  Sidekiq.configure_server do |config|
-    config.redis = { :size => 5 }
   end
 end
