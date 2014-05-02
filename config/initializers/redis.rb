bd7224e5078c6b24613913acf5cc34dc40865ac8
name <email>
2014-05-02 02:32:38 -0400

    Modified unicorn once again to include code that handles workers

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,3 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
-uri = URI.parse("redis://redistogo:5968e16f1b51903c16e949f7a9dd1863@grideye.redistogo.com:10683")
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/")
+# uri = URI.parse("redis://redistogo:5968e16f1b51903c16e949f7a9dd1863@grideye.redistogo.com:10683")
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
