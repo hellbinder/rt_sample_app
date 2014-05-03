2e9e7aac17ccc07d58887bfd691867a93dad0ef4
name <email>
2014-05-03 02:08:37 -0400

    Configured unicorn.rb. Added URL key to the Sidekiq configuration

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,9 +7,9 @@ worker_processes 3
 
 after_fork do |server, worker|
   Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
+    config.redis = { :size => 1, :url => 'redis://redistogo:5968e16f1b51903c16e949f7a9dd1863@grideye.redistogo.com:10683' }
   end
   Sidekiq.configure_server do |config|
-    config.redis = { :size => 2 }
+    config.redis = { :size => 2, :url => redis://redistogo:5968e16f1b51903c16e949f7a9dd1863@grideye.redistogo.com:10683 }
   end
 end
